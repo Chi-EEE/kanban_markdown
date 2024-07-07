94d01c36b25846a891b4071f94eaf00415e58b53
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-07-07 22:42:01 +0100

    Move parser internals to internal.hpp

---
+++ README.md
@@ -0,0 +1,11 @@
+Uses: [XMake](https://xmake.io/#/guide/installation)
+
+Very simple C++ project which uses:
+- fmt - String Formatter
+- re2 - Regex
+- md4c - Markdown Parser
+- tl_expected - expected data structure
+- ordered_map - tsl ordered map which keeps the order of insertion
+
+This project was made quickly since I saw that there were no kanban markdown projects which had the features of Trello.  
+I might continue this project in the future.
--- include/kanban_markdown/reader.hpp
+++ include/kanban_markdown/reader.hpp
@@ -12,72 +12,13 @@
 
 #include <re2/re2.h>
 #include <md4c.h>
-#include <magic_enum_all.hpp>
 
 #include "kanban.hpp"
 #include "constants.hpp"
+#include "internal.hpp"
 
 namespace kanban_markdown {
-	static constexpr uint32_t hash(const std::string_view s) noexcept
-	{
-		uint32_t hash = 5381;
-
-		for (const char* c = s.data(); c < s.data() + s.size(); ++c)
-			hash = ((hash << 5) + hash) + (unsigned char)*c;
-
-		return hash;
-	}
-
-	enum class KanbanState {
-		None = 0,
-		Labels,
-		Board,
-	};
-
-	enum class TaskReadState {
-		None = 0,
-		Description,
-		Labels,
-		Attachments,
-		Checklist,
-	};
-
-	struct LabelDetail {
-		std::string name;
-		std::vector<std::string> list_items;
-	};
-
-	struct LabelSection {
-		std::string current_label_name;
-		tsl::ordered_map<std::string, LabelDetail> label_details;
-	};
-
-	struct Attachment {
-		std::string name;
-		std::string url;
-	};
-
-	struct TaskDetail {
-		std::string name;
-		std::vector<std::string> description;
-		std::vector<std::string> labels;
-		std::vector<Attachment> attachments;
-		std::vector<KanbanChecklistItem> checklist;
-
-		Attachment* currentAttachment;
-	};
-
-	struct BoardSection {
-		std::string name;
-		std::string current_task_name;
-		tsl::ordered_map<std::string, TaskDetail> task_details;
-	};
-
-	struct BoardListSection {
-		std::vector<BoardSection> boards;
-		BoardSection* current_board = nullptr;
-		TaskReadState task_read_state = TaskReadState::None;
-	};
+	using namespace internal;
 
 	struct KanbanParser {
 		std::vector<unsigned int> previous_headers;
