4083b4df1a26952746fdce5e77324c6af1141f73
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-07-13 12:12:12 +0100

    Cleaner code and fix add list button

--- extension/media/kanban.js
+++ extension/media/kanban.js
@@ -37,226 +37,256 @@ $(document).ready(function () {
     const vscode = acquireVsCodeApi();
 
     window.addEventListener('message', event => {
-        const message = event.data.text;
         /** @type {KanbanBoard} */
-        const kanban_board = message.kanban_board;
+        const kanban_board = event.data.text;
+        loadKanbanBoard(kanban_board);
     });
 
-    const $addListButton = $('#add-list');
-    $addListButton.on("click", function () {
-        $addListButton.hide();
+    /**
+     * @param {KanbanBoard} board 
+     */
+    function loadKanbanBoard(board) {
+        const $board = $('#board').empty();
 
-        const $board = $('#board');
+        board.lists.forEach((list, index) => {
+            const $newList = createListElement(list, index + 1);
+            $board.append($newList);
+        });
 
-        const $newList = $('<div>').addClass('list');
-        const listId = 'list-' + ($board.find('.list').length + 1); // Generate unique list ID
+        const $addListButton = $('<button>').attr('id', 'add-list').text('Add another list +');
+        $board.append($addListButton);
+        bindAddListButtonEvent($addListButton);
+    }
+
+    function createListElement(list, listIndex) {
+        const $newList = $('<div>').addClass('list').attr('id', `list-${listIndex}`);
 
         const $listTitle = $('<input>')
             .addClass('list-title')
-            .attr('placeholder', 'Enter list title');
+            .attr('placeholder', 'Enter list title')
+            .val(list.name);
 
-        let isListTitleChanged = false;
-        $listTitle.on('input', function () {
-            isListTitleChanged = true;
-        });
-
-        const $cards = $('<div>')
-            .addClass('cards')
-            .sortable({
-                connectWith: '.cards',
-                tolerance: 'pointer'
-            }).hide();
-
-        const $addCardButton = $('<button>')
-            .addClass('add-card')
-            .text('Add another card +')
-            .on('click', function () {
-                $addCardButton.hide();
-                const $card = $('<div>').addClass('card');
-
-                $card.on('click', function () {
-                    if (isCardTitleChanged) {
-                        $('#edit-card-title').val($cardTitleInput.val());
-                        $('#edit-card-description').val('');
-                        $('#card-modal').css('display', 'block');
-                        $('#save-card').on('click', function () {
-                            $cardTitleInput.val($('#edit-card-title').val());
-                            $('#card-modal').css('display', 'none');
-                        });
-                    }
-                });
-
-                const $cardTitleInput = $('<input>')
-                    .addClass('card-title')
-                    .attr('placeholder', 'Enter card title');
-
-                let isCardTitleChanged = false;
-                $cardTitleInput.on('input', function () {
-                    isCardTitleChanged = true;
-                });
-
-                $cardTitleInput.on('blur', function () {
-                    if (!isCardTitleChanged) {
-                        $card.remove();
-                    } else {
-                        $cardTitleInput.prop('readonly', true);
-                    }
-                    $addCardButton.show();
-                });
-
-                $cardTitleInput.on('keypress', function (e) {
-                    if (e.which === 13) { // Enter key
-                        $cardTitleInput.trigger('blur');
-                    }
-                });
-
-                const $cardMenuButton = $('<button>')
-                    .addClass('card-menu')
-                    .text('⋮')
-                    .on('click', function (event) {
-                        event.stopPropagation();
-                        closeAllMenus();
-                        const $actionsMenu = $card.find('.card-menu-actions');
-                        $actionsMenu.toggle();
-                    });
-
-                const $cardMenuActions = $('<div>')
-                    .addClass('card-menu-actions')
-                    .css('display', 'none');
-
-                const $editCardButton = $('<button>')
-                    .addClass('edit-card')
-                    .text('Edit')
-                    .on("click", function (event) {
-                        event.stopPropagation();
-                        $('#edit-card-title').val($cardTitleInput.val());
-                        $('#edit-card-description').val('');
-                        $('#card-modal').css('display', 'block');
-                        $cardTitleInput.prop('readonly', false).focus();
-
-                        // When Save button in modal is clicked
-                        $('#save-card').one('click', function () {
-                            $cardTitleInput.val($('#edit-card-title').val());
-                            $('#card-modal').css('display', 'none');
-                        });
-                    });
-
-                $cardMenuActions.append($editCardButton);
-
-                $card.append($cardTitleInput, $cardMenuButton, $cardMenuActions);
-                $cards.append($card);
-                $cardTitleInput.trigger('focus');
-            }).hide();
-
-        $listTitle.on('blur', function () {
-            if (!isListTitleChanged) {
-                $newList.remove();
-            } else {
-                vscode.postMessage({
-                    command: 'addList',
-                    listId: listId,
-                    listTitle: $listTitle.val()
-                });
-            }
-            $addListButton.show();
-            $addCardButton.show()
-            $cards.show();
-        });
-
-        $listTitle.on('keypress', function (e) {
-            if (e.which === 13) { // Enter key
-                $listTitle.trigger('blur');
-            }
-        });
-
-        const $listActionsButton = $('<button>')
-            .addClass('list-actions')
-            .text('⋮')
-            .on("click", function (event) {
-                event.stopPropagation();
-                closeAllMenus();
-                const $listActionsMenu = $newList.find('.list-actions-menu');
-                $listActionsMenu.toggle();
-            });
-
-        const $listActionsMenu = $('<div>')
-            .addClass('list-actions-menu')
-            .css('display', 'none');
-
-        const $removeListButton = $('<button>')
-            .addClass('remove-list')
-            .text('Remove list')
-            .on('click', function () {
-                $newList.remove();
-            });
-
-        $listActionsMenu.append($removeListButton);
-
-        $newList.attr('id', listId);
-        $newList.append($listTitle, $listActionsButton, $listActionsMenu, $cards, $addCardButton);
-        $board.children('#add-list').before($newList);
-        $listTitle.trigger('focus');
-        $cards.sortable({
+        const $cards = $('<div>').addClass('cards').sortable({
             connectWith: '.cards',
             tolerance: 'pointer'
         });
-    });
 
-    // Background color picker
+        list.tasks.forEach(task => {
+            const $card = createCardElement(task);
+            $cards.append($card);
+        });
+
+        const $addCardButton = createAddCardButton($cards);
+        const $listActionsButton = createListActionsButton($newList);
+
+        $newList.append($listTitle, $listActionsButton, $cards, $addCardButton);
+        return $newList;
+    }
+
+    /**
+     * @param {Task} task 
+     * @returns 
+     */
+    function createCardElement(task) {
+        const $card = $('<div>').addClass('card');
+
+        const $cardTitleInput = $('<input>')
+            .addClass('card-title')
+            .attr('placeholder', 'Enter card title')
+            .val(task.name)
+            .prop('readonly', true);
+
+        const $cardMenuButton = createCardMenuButton($card);
+        const $cardMenuActions = createCardMenuActions($cardTitleInput);
+
+        $card.append($cardTitleInput, $cardMenuButton, $cardMenuActions);
+        return $card;
+    }
+
+    function createCardMenuButton($card) {
+        return $('<button>')
+            .addClass('card-menu')
+            .text('⋮')
+            .on('click', function (event) {
+                event.stopPropagation();
+                closeAllMenus();
+                $card.find('.card-menu-actions').toggle();
+            });
+    }
+
+    function createCardMenuActions($cardTitleInput) {
+        const $cardMenuActions = $('<div>').addClass('card-menu-actions').hide();
+
+        const $editCardButton = $('<button>')
+            .addClass('edit-card')
+            .text('Edit')
+            .on('click', function (event) {
+                event.stopPropagation();
+                editCard($cardTitleInput);
+            });
+
+        $cardMenuActions.append($editCardButton);
+        return $cardMenuActions;
+    }
+
+    function createAddCardButton($cards) {
+        return $('<button>')
+            .addClass('add-card')
+            .text('Add another card +')
+            .on('click', function () {
+                $(this).hide();
+                const $card = createEditableCard();
+                $cards.append($card);
+                $card.find('.card-title').trigger('focus');
+            });
+    }
+
+    function createEditableCard() {
+        const $card = $('<div>').addClass('card');
+        const $cardTitleInput = $('<input>').addClass('card-title').attr('placeholder', 'Enter card title');
+        let isCardTitleChanged = false;
+
+        $cardTitleInput.on('input', () => isCardTitleChanged = true);
+        $cardTitleInput.on('blur', function () {
+            if (!isCardTitleChanged) {
+                $card.remove();
+            } else {
+                $(this).prop('readonly', true);
+            }
+            $('.add-card').show();
+        });
+
+        $cardTitleInput.on('keypress', function (e) {
+            if (e.which === 13) $(this).blur(); // Enter key
+        });
+
+        const $cardMenuButton = createCardMenuButton($card);
+        const $cardMenuActions = createCardMenuActions($cardTitleInput);
+
+        $card.append($cardTitleInput, $cardMenuButton, $cardMenuActions);
+        return $card;
+    }
+
+    function createListActionsButton($newList) {
+        const $listActionsButton = $('<button>')
+            .addClass('list-actions')
+            .text('⋮')
+            .on('click', function (event) {
+                event.stopPropagation();
+                closeAllMenus();
+                $newList.find('.list-actions-menu').toggle();
+            });
+
+        const $listActionsMenu = $('<div>').addClass('list-actions-menu').hide();
+        const $removeListButton = $('<button>')
+            .addClass('remove-list')
+            .text('Remove list')
+            .on('click', () => $newList.remove());
+
+        $listActionsMenu.append($removeListButton);
+        $listActionsButton.append($listActionsMenu);
+        return $listActionsButton;
+    }
+
+    function bindAddListButtonEvent($addListButton) {
+        $addListButton.on('click', function () {
+            $(this).hide();
+            const $board = $('#board');
+            const $newList = createNewList($board);
+            $board.children('#add-list').before($newList);
+            $newList.find('.list-title').trigger('focus');
+        });
+    }
+
+    function createNewList($board) {
+        const listId = `list-${$board.find('.list').length + 1}`;
+        const $newList = $('<div>').addClass('list').attr('id', listId);
+        const $listTitle = $('<input>').addClass('list-title').attr('placeholder', 'Enter list title');
+        let isListTitleChanged = false;
+
+        $listTitle.on('input', () => isListTitleChanged = true);
+        $listTitle.on('blur', function () {
+            if (!isListTitleChanged) {
+                $newList.remove();
+            }
+            $('#add-list').show();
+            $newList.find('.add-card').show();
+            $newList.find('.cards').show();
+        });
+
+        $listTitle.on('keypress', function (e) {
+            if (e.which === 13) $(this).blur(); // Enter key
+        });
+
+        const $cards = $('<div>').addClass('cards').sortable({
+            connectWith: '.cards',
+            tolerance: 'pointer'
+        }).hide();
+
+        const $addCardButton = createAddCardButton($cards);
+        const $listActionsButton = createListActionsButton($newList);
+
+        $newList.append($listTitle, $listActionsButton, $cards, $addCardButton);
+        return $newList;
+    }
+
+    function editCard($cardTitleInput) {
+        $('#edit-card-title').val($cardTitleInput.val());
+        $('#edit-card-description').val('');
+        $('#card-modal').show();
+        $cardTitleInput.prop('readonly', false).focus();
+
+        $('#save-card').one('click', function () {
+            $cardTitleInput.val($('#edit-card-title').val());
+            $('#card-modal').hide();
+        });
+    }
+
+    function closeAllMenus() {
+        $('.card-menu-actions, .list-actions-menu').hide();
+    }
+
     $('#background-color-picker').on('input', function (event) {
         $('body').css('background-color', event.target.value);
     });
 
-    // Modal functionality
     const $modal = $('#card-modal');
-    $('.close').on('click', function () {
-        $modal.css('display', 'none');
-    });
+    $('.close').on('click', () => $modal.hide());
 
     $(window).on('click', function (event) {
-        if ($(event.target).is($modal)) {
-            $modal.css('display', 'none');
-        }
+        if ($(event.target).is($modal)) $modal.hide();
         closeAllMenus();
     });
 
-    function closeAllMenus() {
-        $('.card-menu-actions, .list-actions-menu').css('display', 'none');
-    }
-
-    // Save card button in modal
-    $('#save-card').on("click", function () {
+    $('#save-card').on('click', function () {
         const cardTitle = $('#edit-card-title').val();
 
         $('.card').each(function () {
-            const $card = $(this);
-            const $cardTitleInput = $card.find('.card-title');
+            const $cardTitleInput = $(this).find('.card-title');
             if ($cardTitleInput.val() === cardTitle) {
                 $cardTitleInput.val(cardTitle);
             }
         });
 
         $('.list').each(function () {
-            const $list = $(this);
-            const $listTitleInput = $list.find('.list-title');
+            const $listTitleInput = $(this).find('.list-title');
             if ($listTitleInput.val() === cardTitle) {
                 $listTitleInput.val(cardTitle);
             }
         });
 
-        $modal.css('display', 'none');
+        $modal.hide();
     });
 
-    // Edit kanban title
     const $kanbanTitle = $('#kanban-title');
     const $editTitleInput = $('#edit-title-input');
 
-    $kanbanTitle.on("click", function () {
+    $kanbanTitle.on('click', function () {
         $kanbanTitle.hide();
-        $editTitleInput.show().val($kanbanTitle.text().trim()).focus();
+        $editTitleInput.show().val($kanbanTitle.text().trim()).trigger('focus');
     });
 
-    $editTitleInput.on("blur", function () {
+    $editTitleInput.on('blur', function () {
         const newTitle = $editTitleInput.val().trim();
         if (newTitle === '') {
             $editTitleInput.val($kanbanTitle.text().trim());
