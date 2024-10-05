3b8246f803382a9b8b76efdcf47a9226102e670f
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-10-05 17:41:13 +0100

    Update example

--- wasm/main.cpp
+++ wasm/main.cpp
@@ -1,19 +1,24 @@
 #include <emscripten/bind.h>
 #include <string>
+#include <tl/expected.hpp>
 
-class MyClass {
+class MyClass
+{
 public:
     MyClass() : value(0) {}
 
-    void setValue(int val) {
+    void setValue(int val)
+    {
         value = val;
     }
 
-    int getValue() const {
+    int getValue() const
+    {
         return value;
     }
 
-    std::string greet() const {
+    std::string greet() const
+    {
         return "Hello from MyClass!";
     }
 
@@ -21,11 +26,45 @@ private:
     int value;
 };
 
-// Bindings
-EMSCRIPTEN_BINDINGS(my_class_example) {
+template <typename T>
+struct Ok
+{
+    const T value;
+    const bool ok = true;
+};
+
+struct Err
+{
+    const std::string error;
+    const bool ok = false;
+};
+
+// Function to create MyClass using tl::expected and return MyClassResult
+emscripten::val createMyClass(int value)
+{
+    if (value < 0)
+        return emscripten::val(Err{"Value cannot be negative"});
+    MyClass obj;
+    obj.setValue(value);
+    return emscripten::val(Ok<MyClass>{obj});
+}
+
+// Binding code
+EMSCRIPTEN_BINDINGS(my_class_example)
+{
     emscripten::class_<MyClass>("MyClass")
         .constructor()
         .function("setValue", &MyClass::setValue)
         .function("getValue", &MyClass::getValue)
         .function("greet", &MyClass::greet);
+
+    emscripten::class_<Ok<MyClass>>("Ok")
+        .property("value", &Ok<MyClass>::value)
+        .property("ok", &Ok<MyClass>::ok);
+
+    emscripten::class_<Err>("Err")
+        .property("error", &Err::error)
+        .property("ok", &Err::ok);
+
+    emscripten::function("createMyClass", &createMyClass);
 }
